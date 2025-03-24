86ca06b076e23bdacdc751fc019489c70f38e0e8
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-03-24 17:52:42 +0100

    First commit

--- src/components/TicketForm.tsx
+++ src/components/TicketForm.tsx
@@ -149,7 +149,6 @@ const Step3 = ({ step, setStep }) => {
       <h1>Your Ticket is Booked</h1>
       <p>Check your email for a copy or you can <strong>download</strong></p>
       <Subtract />
-      <p>I'm trying to push my code to the repository</p>
     </div>
   )
 }
